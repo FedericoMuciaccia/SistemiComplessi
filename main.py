7265ad4a4da3397f91a95a8ee0d1aeef4e260800
FedericoMuciaccia <fedem88@hotmail.it>
2016-01-31 20:14:12 +0100

    aggiunto file diff per prove

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 
 print("Sistemi Complessi!")
 
-import pandas
+# dato che ci serve matplotlib useremo python 2
 
+import numpy
+import pandas
+from matplotlib import pyplot as plt
+
+dataframe = pandas.read_cvs("cell_towers_diff-2016012100.csv")
+
+# %matplotlib inline
+
+plt.figure(); df.plot(); plt.legend(loc='best')
 
