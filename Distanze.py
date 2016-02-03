347ee4dbd226b6052216b4ea3c98c8eafce71d89
Iuri La Rosa <iuri.larosa.88@gmail.com>
2016-02-03 12:13:13 +0100

    sistemato errore, impostato il todo di stasera

--- Distanze.py
+++ Distanze.py
@@ -1,28 +1,53 @@
+
+import geopy
+from geopy import distance #TODO BUGGONE
 import math
 import itertools
 
-xcentro = 41.891969
-ycentro = 12.495644
-longitudini = [41.914456, 41.990672, 41.793883, 41.812566, 41.956277]
-latitudini = [12.615807, 12.502714, 12.511297, 12.396628, 12.384611]
+colosseo = (41.890173, 12.492331)
+raccordo = [(41.914456, 12.615807),(41.990672, 12.502714),(41.793883, 12.511297),(41.812566, 12.396628),(41.956277, 12.384611)]
+raggi = []
+
+def geodesicDistance(A, B=colosseo):
+    return geopy.distance.vincenty(A, B).meters
+
+raggi = map(geodesicDistance, raccordo)
+
+raggiomedio = 0
+for i in raggi:
+	raggiomedio += i
+raggiomedio /= len(distanza)
+raggiomedio = 11000
+#print distanza
+#print media
+
+import pandas
+dataframe = pandas.read_csv("copiaprova.csv")
+#dataframe
+
+
 distanza = []
+#dataframe['lon'] == "Noise - Street/Sidewalk"
+coordinate = dataframe[dataframe.lat > 0][dataframe.lon > 0][['lon', 'lat']].values
+#inroma = pandas.DataFrame([[41.947416, 12.371001],
+#                            [41.899392, 12.397436],
+#                            [41.870510, 12.287917],
+#                            [41.899648, 12.515196]], 
+#                            columns=('lon', 'lat'))
 
-for (x,y) in zip(longitudini,latitudini):
-    raggio = math.sqrt((xcentro-x)**2+(ycentro-y)**2)
-    distanza.append(raggio)
 
-media = 0
-for i in distanza:
-	media += i
-media /= len(distanza)
+distanza = map(geodesicDistance, coordinate)
 
-print media 
 
-def distanza(x, y)
-    d = math.sqrt((x-xcentro)**2+(y-ycentro)**2)
-    return d
+def isInRome(point):
+    return geodesicDistance(point) <= raggiomedio
+#latitudini = dataframe[dataframe.lat > 0][['lat']].values
+#longitudini = dataframe[dataframe.lat > 0][['lat']].values 
 
-map(distanza, x, y)
-
-filter(d < raggio, x, y)
-
+#coordinate = (latitudini, longitudini)
+a = isInRome((41.899903, 12.503008))
+print geodesicDistance((41.899903, 12.503008))
+print raggiomedio
+print a
+filter(isInRome, coordinate)
+#isInRome(colosseo)
