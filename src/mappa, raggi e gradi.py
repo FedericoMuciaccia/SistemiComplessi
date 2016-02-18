05ad50e25c3b50144ad8e1f77b282f7ef7b68a14
FedericoMuciaccia <fedem88@hotmail.it>
2016-02-18 01:52:03 +0100

    problemi con l0istogramma

--- src/mappa, raggi e gradi.py
+++ src/mappa, raggi e gradi.py
@@ -1,12 +1,13 @@
 
 # coding: utf-8
 
-# In[16]:
+# In[31]:
 
 import numpy
 import pandas
 from matplotlib import pyplot
 get_ipython().magic(u'matplotlib inline')
+from scipy import stats # TODO vedere perché non fa chiamare il modulo direttamente
 
 import gmaps
 
@@ -16,7 +17,7 @@ import gmaps
 # invece che uno scatterplot con dei raggi, la libreria ci consente solo di fare una heatmap (eventualmente pesata)
 # 
 
-# In[18]:
+# In[5]:
 
 roma = pandas.read_csv("../data/Roma_towers.csv")
 coordinate = roma[['lat', 'lon']].values
@@ -34,7 +35,7 @@ gmaps.heatmap(coordinate)
 # dato che ci servirà fare un grafico con scale logaritmiche eliminiamo i dati con
 # > range = 0
 
-# In[19]:
+# In[6]:
 
 
 # condizioni di filtro
@@ -53,7 +54,7 @@ print max(raggi)
 
 # creazione di un istogramma log-log per la distribuzione del raggio di copertura
 
-# In[20]:
+# In[7]:
 
 
 pyplot.figure(figsize=(20,8)) # dimensioni in pollici
@@ -81,14 +82,14 @@ pyplot.yscale("log")
 # equispaziati nel plot logaritmico (canali pesati)
 
 
-# In[21]:
+# In[8]:
 
 # TODO vedere se il grafo finale non è scale free, 
 # non è reticolo geometrico.
 # è una specie di vetro
 
 
-# In[22]:
+# In[9]:
 
 # Tre = roma.net == 99
 # treCell = roma[Tre].reset_index(drop=True)
@@ -102,7 +103,7 @@ ordinato = troncato.reset_index(drop=True)
 ordinato
 
 
-# In[23]:
+# In[10]:
 
 
 # criteri di filtro per le compagnie telefoniche
@@ -119,7 +120,7 @@ tre = roma[isTre].reset_index(drop=True)
  
 
 
-# In[26]:
+# In[11]:
 
 tim = romaFiltrato[isTim].reset_index(drop=True)
 wind = romaFiltrato[isWind].reset_index(drop=True)
@@ -127,7 +128,7 @@ vodafone = romaFiltrato[isVodafone].reset_index(drop=True)
 tre = romaFiltrato[isTre].reset_index(drop=True)
 
 
-# In[31]:
+# In[12]:
 
 compagnie = ['Tim','Wind','Vodafone','Tre','aggregati']
 
@@ -159,7 +160,7 @@ pyplot.legend()
 # TODO (futuristico):
 # vedere se esiste un modo per mettere due o più computer in rete a fare una piccola grid, per condividere la potenza di calcolo, la memoria ram e lo spazio su disco
 
-# In[29]:
+# In[13]:
 
 pyplot.figure(figsize=(20,8))
 distribuzioneRange = pyplot.hist(raggi.values,                                 bins=div,                                 histtype='step')
@@ -168,58 +169,58 @@ pyplot.xscale("log")
 pyplot.yscale("log")
 
 
-# In[385]:
+# In[14]:
 
 a,b,c = distribuzioneRange
 
 
-# In[402]:
+# In[15]:
 
 a1 = numpy.append(a.astype(int), 0)
 
 
-# In[403]:
+# In[16]:
 
 b1 = b.astype(int)
 
 
-# In[404]:
+# In[17]:
 
 a1
 
 
-# In[405]:
+# In[18]:
 
 b1
 
 
-# In[406]:
+# In[19]:
 
 div = numpy.logspace(start=0, stop=15, num=16, base=2).astype(int) # max = 20341
 div
 
 
-# In[411]:
+# In[20]:
 
 a2 = a1/div
 a2
 
 
-# In[414]:
+# In[21]:
 
 pyplot.scatter(y=a2,x=div)
 pyplot.xscale("log")
 pyplot.yscale("log")
 
 
-# In[1]:
+# In[22]:
 
 # TODO vedere se le due slope del grafico log-log possono essere
 # messe in relazione con le due slode della legge
 # di Heap e di Zipf (o come cavolo si scrivono)
 
 
-# In[2]:
+# In[23]:
 
 # TODO esterno:
 
@@ -232,7 +233,7 @@ pyplot.yscale("log")
 # che al mercato mio padre comprò
 
 
-# In[3]:
+# In[24]:
 
 # TODO
 # fit e lot della distribuzione del grado con i due modelli di rete
@@ -240,9 +241,11 @@ pyplot.yscale("log")
 # preferential attachement di Barabasi Albert
 
 
-# ## plot unificato della distribuzione del grado
+# ## distribuzione del grado
+# 
+# si fa un plot logaritmico unificato e poi i relativi fit con i vari modelli di rete
 
-# In[46]:
+# In[25]:
 
 
 gradi = [numpy.loadtxt("../data/DistrGrado_{0}".format(compagnia)) for compagnia in compagnie]
@@ -253,7 +256,7 @@ gradi = [numpy.loadtxt("../data/DistrGrado_{0}".format(compagnia)) for compagnia
 # grado = numpy.loadtxt("../data/DistrGrado_{0}",compagnia)
 
 
-# In[50]:
+# In[26]:
 
 pyplot.figure(figsize=(20,8)) # dimensioni in pollici
 distribuzioneRangeTutti = pyplot.hist(gradi,                                 bins=max(gradi[-1])-min(gradi[-1]),                                 histtype='step',                                 label=compagnie,                                 color=colori
@@ -268,10 +271,205 @@ pyplot.yscale("log")
 pyplot.legend()
 
 
-# In[40]:
+# ###  altro approccio, con i dataframe
 
-gradi
+# In[27]:
 
+dictGradi = dict()
+for label in compagnie:
+    dictGradi[label] = numpy.loadtxt("../data/DistrGrado_{0}".format(label)).astype(int)
+
+dictGradi
+
+
+# In[28]:
+
+
+# TODO vedere se c'è un metodo migliore rispetto a transpose()
+dataframeGradi = pandas.DataFrame.from_dict(dictGradi, orient='index').transpose()
+# TODO attenzione ai valori NaN nei posti vuoti
+
+
+# In[41]:
+
+canali = int(max(dataframeGradi.aggregati)-min(dataframeGradi.aggregati))
+
+kwds = {
+    "bins": canali,
+    "histtype": 'step',
+    # "color": colori,
+    #"fill": True,
+    #"alpha": 0.25,
+    #"linestyle": "solid",
+    #"edgecolor": "black"
+}
+
+dataframeGradi.plot(kind='hist', loglog=True, figsize=(20,8), **kwds)
+
+
+# In[30]:
+
+abc = numpy.random.normal(0,1,1000)
+# pyplot.hist(abc, bins=100)
+
+
+# In[172]:
+
+mu, sigma = scipy.stats.norm.fit(abc)
+
+
+# In[173]:
+
+x = numpy.linspace(-4, 4, 1000)
+y = scipy.stats.norm.pdf(x, mu, sigma)
+pyplot.hist(abc, bins=100, normed=True)
+pyplot.plot(x, y)
+pyplot.show()
+
+
+# In[174]:
+
+scipy.optimize.curve_fit(scipy.stats.norm.pdf, x, abc)
+
+
+# In[182]:
+
+import math
+
+def poissoniana(x, mu):
+    return (numpy.exp(-mu) * mu**x)/ (scipy.misc.factorial(x)) # if x >= 0
+
+
+# In[184]:
+
+x = numpy.linspace(1,1000,1000)
+# y = poissoniana(x, 3)
+y = dataframeGradi.Tre.values
+
+
+# In[ ]:
+
+popt, pcov = scipy.optimize.curve_fit(poissoniana, x, y)
+
+
+# In[201]:
+
+y, x = numpy.histogram(dataframeGradi.aggregati.values, bins = 3)
+
+
+# In[202]:
+
+min(dataframeGradi.aggregati.values)
+
+
+# In[203]:
+
+len(y),len(x)
+
+
+# In[204]:
+
+y,x
+
+
+# In[229]:
+
+arr = [5,5,5,5,5,6,6,6,7,7]
+
+y, x = numpy.histogram(numpy.array(arr), bins=(5,6,7,8))
+
+
+# In[230]:
+
+y,x
+
+
+# In[232]:
+
+pyplot.hist(arr, bins=(5,6,7,8))
+
+
+# In[234]:
+
+pyplot.hist(arr, bins=3)
+
+
+# In[238]:
+
+pyplot.hist(arr, bins=3, align="left")
+
+
+# In[240]:
+
+pyplot.hist(arr, bins=(5,6,7,8), align="mid")
+
+
+# In[241]:
+
+pyplot.hist(arr, bins=(5,6,7,8), align="left")
+
+
+# In[245]:
+
+def integerHistogram(integerData):
+    integerData = [5,5,5,5,5,6,6,6,7,7]
+    minimum = min(integerData)
+    maximum = max(integerData)
+    integerBins = maximum - minimum + 1
+    numberSet = numpy.linspace(minimum, maximum, integerBins)
+    y, x = numpy.histogram(arr, bins=(5,6,7,8))
+    
+
+
+# In[246]:
+
+numpy.histogram(arr, bins=(5,6,7,8))
+
+
+# In[265]:
+
+integerData = [5,5,5,5,5,6,6,6,7,7,9,10,10]
+minimum = min(integerData)
+maximum = max(integerData)
+integerBins = maximum - minimum + 1
+numberSet = numpy.linspace(minimum, maximum, integerBins, dtype=int)
+min(integerData), max(integerData)
+
+
+# In[260]:
+
+numberSet
+
+
+# In[262]:
+
+numpyBins = numberSet + [minimum+1]
+
+
+# In[263]:
+
+numpyBins
+
+
+# 
+# http://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.transpose.html
+# http://stackoverflow.com/questions/17779316/un-normalized-gaussian-curve-on-histogram
+# !!!    http://glowingpython.blogspot.it/2012/07/distribution-fitting-with-scipy.html
+# http://danielhnyk.cz/fitting-distribution-histogram-using-python/
+# http://stackoverflow.com/questions/19736080/creating-dataframe-from-a-dictionary-where-entries-have-different-lengths
+# !!!    https://plot.ly/pandas/histograms/
+# https://www.python.org/dev/peps/pep-0008/
+# http://stackoverflow.com/questions/1769403/understanding-kwargs-in-python
+# http://docs.scipy.org/doc/scipy/reference/stats.html
+# http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.poisson.html#scipy.stats.poisson
+# http://stackoverflow.com/questions/3433486/how-to-do-exponential-and-logarithmic-curve-fitting-in-python-i-found-only-poly
+# http://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.bincount.html#numpy.bincount
+# http://docs.scipy.org/doc/numpy-1.10.0/reference/generated/numpy.linspace.html
+# 
+# 
+# 
+# 
+# 
 
 # In[ ]:
 
