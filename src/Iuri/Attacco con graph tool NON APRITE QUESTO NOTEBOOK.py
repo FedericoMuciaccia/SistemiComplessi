4c88ab5676dd83d06a14650dae14012906a1445e
Iuri La Rosa <iuri.larosa.88@gmail.com>
2016-03-10 23:10:05 +0100

    finita analisi topologica percolante!

--- src/Iuri/Attacco con graph tool NON APRITE QUESTO NOTEBOOK.py
+++ src/Iuri/Attacco con graph tool NON APRITE QUESTO NOTEBOOK.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
+# <markdowncell>
+
+# #Attacco e failure con seaborn
+
+# <markdowncell>
+
+# ###Importo librerie e definisco funzioni fondamentali
+
 # <codecell>
 
 from graph_tool.all import *
@@ -11,6 +19,12 @@ import numpy
 import pandas
 import math
 #import seaborn
+
+
+gestore = ["Tim", "Vodafone", "Wind", "Tre", "Roma"]
+colori = ['#004184','#ff3300','#ff8000','#018ECC', '#4d4d4d']
+
+# <codecell>
 
 #Costruzione grafo con graph-tool da matrice di adiacenza
 def conversione(grafo, adiacenza):
@@ -27,10 +41,10 @@ def costruzione(gestore):
     conversione(grafo, adiacenza)
     grafo.save("GTool{0}.xml".format(gestore))
     #graph_tool.draw.graph_draw(grafoTre)
-gestore = ["Tim", "Vodafone", "Wind", "Tre", "Roma"]
 
 # <codecell>
 
+#funzioni topologiche
 def averageLength(grafo):
     istoLength = graph_tool.stats.distance_histogram(grafo)
     istoLength[1] = numpy.delete(istoLength[1], len(istoLength[1])-1)
@@ -45,36 +59,47 @@ def clustering(grafo):
     array = numpy.array(cluster.a)
     return numpy.average(array)
 
-def averageGrado(grafo):
-    kmedio = graph_tool.stats.vertex_average(grafo, "total")
-    return kmedio[0]
+# <codecell>
 
-def criterion(grafo):
-    kmedio = graph_tool.stats.vertex_average(grafo, "total")
-    kmedioQuadro = numpy.power(kmedio, 2)
-    criterion = (kmedioQuadro[0]+kmedioQuadro[1])/(kmedio[0])
-    return criterion
-
-def topologia(grafo):
-    d = diameter(grafo)
-    l = averageLength(grafo)
-    C = clustering(grafo)
-    k = averageGrado(grafo)
-    c = criterion(grafo)
-    return (d, l), C, (k, c)
+def gradonodo(identificativo):
+        vertice = grafo.vertex(identificativo)
+        return vertice.out_degree()
+indice = numpy.arange(grafo.num_vertices())
+listagradi = map(gradonodo, indice)
 
 # <codecell>
 
+def kquadromedio(listadeg):
+    sommatoriaquadra = 0.0
+    for grado in listadeg:
+        sommatoriaquadra = sommatoriaquadra + grado**2    
+    return sommatoriaquadra/len(listadeg)
+
+criterion = kquadromedio(listadeg)/averageGrado(grafo)
+%time kquadromedio(listagradi)
+
+# <markdowncell>
+
+# ### Cicli input/output gafi
+
+# <codecell>
+
+#NON ESEGUIRE, reti già costruite
 for compagnia in gestore:
     %time costruzione(compagnia)
 
 # <codecell>
 
+# caricamento rete e studio topologico iniziale
 for compagnia in gestore:
     print compagnia
     grafo = load_graph("/home/protoss/Documenti/Siscomp_datas/data/GTool{0}.xml".format(gestore))
     topo = %time topologia(grafo)
     print topo, "\n"
+
+# <markdowncell>
+
+# # Simulazione attacco, andamento grandezze topologiche in funzione dei nodi rimossi
 
 # <codecell>
 
@@ -82,8 +107,19 @@ for compagnia in gestore:
 def attackPercent(compagnia, steps):
     grafoFinal = load_graph("/home/protoss/Documenti/Siscomp_datas/data/GTool{0}.xml".format(compagnia))
 
+    def gradonodo(identificativo):
+        vertice = grafoFinal.vertex(identificativo)
+        return vertice.out_degree()
+
+    def kmedio(listadeg):
+        return numpy.mean(listadeg)
+    
+    def kquadromedio(listadeg):
+        listadegQuadri = numpy.power(listadeg, 2)
+        return numpy.mean(listadegQuadri)
+    
     graphSize = grafoFinal.num_vertices()
-    passo = graphSize/float(steps)
+    passo = graphSize/steps
 
     i = 0
     ascisse.append(i)
@@ -92,42 +128,118 @@ def attackPercent(compagnia, steps):
     diametro.append(diameter(grafoFinal))
     cammino.append(averageLength(grafoFinal))
     cluster.append(clustering(grafoFinal))
-    gradomedio.append(averageGrado(grafoFinal))
-    criterio.append(criterion(grafoFinal))
+    
+    indice = numpy.arange(graphSize)
+    listaGradi = map(gradonodo, indice)
+
+    gradomedio.append(kmedio(listaGradi))
+    criterion = kquadromedio(listaGradi)/kmedio(listaGradi)
+    criterio.append(criterion)
     relSizeGC.append(1)
     
-    
-    # PROBLEMA: BISOGNA FARE IN MODO CHE NON CRASHI
-    while (grafoFinal.num_vertices() > passo):
-        istogradi = graph_tool.stats.vertex_hist(grafoFinal, "total")
-        gradiFinal = pandas.DataFrame(istogradi[0], columns=['grado'])
+    while (grafoFinal.num_vertices() > passo):    
+        gradiFinal = pandas.DataFrame(listaGradi, columns=['grado'])
         gradiFinal = pandas.DataFrame.reset_index(gradiFinal)
         gradiFinal.sort(["grado"], ascending=[False], inplace=True)
         sortedIDnode = gradiFinal['index'].values
 
-        for identificativo in sortedIDnode:
-            if (grafoFinal.num_vertices() > len(sortedIDnode) - passo):
-                grafoFinal.remove_vertex(identificativo)
+        daRimuovere = numpy.take(sortedIDnode, range(passo))
+        grafoFinal.remove_vertex(daRimuovere)
 
         giantCluster = graph_tool.topology.label_largest_component(grafoFinal)
         giantCluster = graph_tool.GraphView(grafoFinal, vfilt=giantCluster)
-
+        grafoFinal = Graph(grafoFinal, prune = True)
+        
+        graphSize = graphSize-passo
         i += 100/steps
         ascisse.append(i)
         aziendaFinal.append(compagnia)
-
-        graphSize = grafoFinal.num_vertices()
+        
+        
         diametro.append(diameter(grafoFinal))
         cammino.append(averageLength(grafoFinal))
         cluster.append(clustering(grafoFinal))
-        gradomedio.append(averageGrado(grafoFinal))
-        criterio.append(criterion(grafoFinal))
         relSizeGC.append((giantCluster.num_vertices())/(float(graphSize)))
+        
+        indice = numpy.arange(grafoFinal.num_vertices())
+        listaGradi = map(gradonodo, indice)
+        
+        gradomedio.append(kmedio(listaGradi))
+        criterion = kquadromedio(listaGradi)/averageGrado(grafoFinal)
+        criterio.append(criterion)
+
+#PERCENT FAILURE!!
+def failurePercent(compagnia, steps):
+    grafoFinal = load_graph("/home/protoss/Documenti/Siscomp_datas/data/GTool{0}.xml".format(compagnia))
+
+    def gradonodo(identificativo):
+        vertice = grafoFinal.vertex(identificativo)
+        return vertice.out_degree()
+
+    def kmedio(listadeg):
+        return numpy.mean(listadeg)
+    
+    def kquadromedio(listadeg):
+        listadegQuadri = numpy.power(listadeg, 2)
+        return numpy.mean(listadegQuadri)
+    
+    graphSize = grafoFinal.num_vertices()
+    passo = graphSize/steps
+
+    i = 0
+    ascisse.append(i)
+    aziendaFinal.append(compagnia)
+
+    diametro.append(diameter(grafoFinal))
+    cammino.append(averageLength(grafoFinal))
+    cluster.append(clustering(grafoFinal))
+    
+    indice = numpy.arange(graphSize)
+    listaGradi = map(gradonodo, indice)
+
+    gradomedio.append(kmedio(listaGradi))
+    criterion = kquadromedio(listaGradi)/kmedio(listaGradi)
+    criterio.append(criterion)
+    relSizeGC.append(1)
+    
+    while (grafoFinal.num_vertices() > passo): 
+        gradiFinal = pandas.DataFrame(listaGradi, columns=['grado'])
+        gradiFinal = pandas.DataFrame.reset_index(gradiFinal)
+        randomante = gradiFinal['index'].values
+        randomante = numpy.random.permutation(randomante)
+
+        daRimuovere = numpy.take(randomante, range(passo))
+        grafoFinal.remove_vertex(daRimuovere)
+
+        giantCluster = graph_tool.topology.label_largest_component(grafoFinal)
+        giantCluster = graph_tool.GraphView(grafoFinal, vfilt=giantCluster)
+        grafoFinal = Graph(grafoFinal, prune = True)
+        
+        graphSize = graphSize-passo
+        i += 100/steps
+        ascisse.append(i)
+        aziendaFinal.append(compagnia)
+        
+        
+        diametro.append(diameter(grafoFinal))
+        cammino.append(averageLength(grafoFinal))
+        cluster.append(clustering(grafoFinal))
+        relSizeGC.append((giantCluster.num_vertices())/(float(graphSize)))
+        
+        indice = numpy.arange(grafoFinal.num_vertices())
+        listaGradi = map(gradonodo, indice)
+        
+        gradomedio.append(kmedio(listaGradi))
+        criterion = kquadromedio(listaGradi)/averageGrado(grafoFinal)
+        criterio.append(criterion)
 
 # <codecell>
 
-#gestore = ["Tim", "Vodafone", "Wind", "Tre"]
-gestore = "Tre"
+#gestore = ["Tim", "Vodafone", "Wind", "Tre", "Roma"]
+gestore = ["Tim", "Vodafone", "Wind", "Tre"]
+#compagnia = "Tre"
+
+indice = []
 diametro = []
 cammino = []
 cluster = []
@@ -138,15 +250,15 @@ relSizeGC = []
 aziendaFinal = []
 ascisse = []
 
-#for compagnia in gestore:
-#    %time attackPercent(compagnia, 10)
+for compagnia in gestore:
+    %time attackPercent(compagnia, 100)
 
-attackPercent(gestore, 10)
+#attackPercent(compagnia, 100)
 
 datiFinal = pandas.DataFrame()
 datiFinal['percent'] = ascisse
 
-datiFinal['provider'] = aziendaFinal
+datiFinal['Provider'] = aziendaFinal
 datiFinal['diameter'] = diametro
 datiFinal['average path length'] = cammino
 datiFinal['clustering'] = cluster
@@ -154,6 +266,207 @@ datiFinal['average degree'] = gradomedio
 datiFinal['soglia percolativa'] = criterio
 
 datiFinal['GCsize'] = relSizeGC
-datiFinal.to_csv("/home/protoss/Documenti/SistemiComplessi/data/Iuri/AttackDataForSeaborn.csv")
+datiFinal.to_csv("/home/protoss/Documenti/SistemiComplessi/data/Iuri/GtoolAttackDataForSeaborn.csv")
 datiFinal.head()
+
+# <codecell>
+
+#gestore = ["Tim", "Vodafone", "Wind", "Tre", "Roma"]
+gestore = ["Tim", "Vodafone", "Wind", "Tre"]
+#gestore = "Roma"
+diametro = []
+cammino = []
+cluster = []
+gradomedio = []
+criterio = []
+relSizeGC = []
+
+aziendaFinal = []
+ascisse = []
+
+for compagnia in gestore:
+    %time failurePercent(compagnia, 100)
+
+#attackPercent(gestore, 50)
+
+datiFinal = pandas.DataFrame()
+datiFinal['percent'] = ascisse
+
+datiFinal['Provider'] = aziendaFinal
+datiFinal['diameter'] = diametro
+datiFinal['average path length'] = cammino
+datiFinal['clustering'] = cluster
+datiFinal['average degree'] = gradomedio
+datiFinal['soglia percolativa'] = criterio
+
+datiFinal['GCsize'] = relSizeGC
+datiFinal.to_csv("/home/protoss/Documenti/SistemiComplessi/data/Iuri/GtoolFailureDataForSeaborn.csv")
+datiFinal.head()
+
+# <codecell>
+
+#Attack
+import seaborn
+%matplotlib inline
+datiFinal = pandas.read_csv('/home/protoss/Documenti/SistemiComplessi/data/Iuri/GtoolAttackDataForSeaborn.csv')
+
+seaborn.set_context("notebook", font_scale=1.1)
+seaborn.set_style("ticks")
+
+#diametro
+seaborn.lmplot('percent', 'diameter', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Attack: diametro')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0, 80)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolAttackD_Final', format='eps', dpi=1000)
+
+#giant cluster
+seaborn.lmplot('percent', 'GCsize', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Attack: dimensioni relative del GC')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,1.1)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolAttackGC_Final', format='eps', dpi=1000)
+
+#average length
+seaborn.lmplot('percent', 'average path length', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Attack: average path length')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,14)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolAttackl_Final', format='eps', dpi=1000)
+
+#clustering
+seaborn.lmplot('percent', 'clustering', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Attack: coefficiente clustering')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,1.1)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolAttackC_Final', format='eps', dpi=1000)
+
+#gradomedio
+seaborn.lmplot('percent', 'average degree', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Attack: average degree')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,100)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolAttackk_Final', format='eps', dpi=1000)
+
+#CRITERION
+seaborn.lmplot('percent', 'soglia percolativa', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Attack: criterion')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,10)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolAttackc_Final', format='eps', dpi=1000)
+
+# <codecell>
+
+#Failure
+import seaborn
+%matplotlib inline
+datiFinal = pandas.read_csv('/home/protoss/Documenti/SistemiComplessi/data/Iuri/GtoolFailureDataForSeaborn.csv')
+
+seaborn.set_context("notebook", font_scale=1.1)
+seaborn.set_style("ticks")
+
+#diametro
+seaborn.lmplot('percent', 'diameter', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Random failure: diametro')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0, 20)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolFailureD_Final', format='eps', dpi=1000)
+
+#giant cluster
+seaborn.lmplot('percent', 'GCsize', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Random failure: dimensioni relative del GC')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,1.1)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolFailureGC_Final', format='eps', dpi=1000)
+
+#average length
+seaborn.lmplot('percent', 'average path length', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Random failure: average path length')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,4)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolFailurel_Final', format='eps', dpi=1000)
+
+#clustering
+seaborn.lmplot('percent', 'clustering', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Random failure: coefficiente clustering')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,1.1)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolFailureC_Final', format='eps', dpi=1000)
+
+#gradomedio
+seaborn.lmplot('percent', 'average degree', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Random failure: average degree')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,100)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolFailurek_Final', format='eps', dpi=1000)
+
+#CRITERION
+seaborn.lmplot('percent', 'soglia percolativa', data=datiFinal, fit_reg=False,
+           size = 7, aspect = 1.7778,
+           hue='Provider', palette = colori,
+           scatter_kws={"marker": "D", "s": 100})
+pyplot.title('Random failure: criterion')
+pyplot.xlabel("%")
+pyplot.ylabel("Valore")
+pyplot.xlim(0, 100)
+pyplot.ylim(0,10)
+pyplot.savefig('/home/protoss/Documenti/SistemiComplessi/img/iuri/gToolFailurec_Final', format='eps', dpi=1000)
+
+# <codecell>
+
 
