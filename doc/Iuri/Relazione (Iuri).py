b32f541adbd12e90b4a1c82ec099c68362b28d20
Iuri La Rosa <iuri.larosa.88@gmail.com>
2016-03-12 05:11:24 +0100

    finita parte teorica, valutare se sintetizzare

--- doc/Iuri/Relazione (Iuri).py
+++ doc/Iuri/Relazione (Iuri).py
@@ -162,16 +162,20 @@
 # ## 3.2.3 Effetto cascata
 # 
 
-# ##Bibliografia
+# ## Bibliografia
 # 
-# 1. [A. L. Barabasi and R. Albert, *Emergence of scaling in random networks*, (1999).](http://arxiv.org/pdf/cond-mat/9910332)  
-# 2. [R. Albert and A. L. Barabasi, *Statistical mechanic of complex networks*, (2001).](http://arxiv.org/pdf/cond-mat/0106096) 
-# 3. [R. Cohen, K. Erez, D. ben-Avraham and S. Havlin, *Resilience of the internet to random breakdown*, (2000).](http://arxiv.org/pdf/cond-mat/0007048)
-# 4. [R. Cohen, K. Erez, D. ben-Avraham and S. Havlin, *Breakdown of the internet under intentional attack*, (2001).](http://arxiv.org/pdf/cond-mat/0010251)
-# 5. [R. Albert, H. Jeong and A. L. Barabasi, *Error and attack tolerance of complex networks*, (2000).](http://www.nature.com/nature/journal/v406/n6794/pdf/406378a0.pdf)
-# 6. [P. Erdős and A. Rényi, *On random graph I.*, (1959).](http://www.renyi.hu/~p_erdos/1959-11.pdf)
-# 7. [J. Watts and H. Strogatz, *Collective dynamics of 'small-world' networks*, (1998).](http://www.nature.com/nature/journal/v393/n6684/pdf/393440a0.pdf)
-# 8. [A. E. Motter and Y. C. Lai, Cascade-based attacks on complex networks, (2003).](http://arxiv.org/pdf/cond-mat/0301086)
+# 1. [Albert, R. and A. L. Barabasi (2002), *Statistical mechanic of complex networks*, Reviews of modern phyisics **74**, p. 47.](http://arxiv.org/pdf/cond-mat/0106096) 
+# 2. [Albert, R., H. Jeong, and A. L. Barabasi (2000), *Error and attack tolerance of complex networks*, Nature **406**, p. 378.](http://www.nature.com/nature/journal/v406/n6794/pdf/406378a0.pdf)
+# 3. [Barabasi, A. L. and R. Albert (1999), *Emergence of scaling in random networks*, Science **286**, p. 509.](http://arxiv.org/pdf/cond-mat/9910332)
+# 4. [Cohen, R. K. Erez, D. ben-Avraham and S. Havlin (2000), *Resilience of the internet to random breakdown*, Physical Review letters **85**, p. 4626.](http://arxiv.org/pdf/cond-mat/0007048)
+# 5. [Cohen, R. K. Erez, D. ben-Avraham and S. Havlin (2001), *Breakdown of the internet under intentional attack*, Physical Review letters **86**, p. 3682.](http://arxiv.org/pdf/cond-mat/0010251)
+# 6. [Erdős, P. and A. Rényi (1959), *On random graph I.*, Publicationes Mathematicae Debrecen **6**, p. 290.](http://www.renyi.hu/~p_erdos/1959-11.pdf)
+# 7. [Molloy, M. e B. Reed (1995). *A critical point for random graphs with a given degree sequence*. Random structures and algorithms **6**, p. 161.](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.24.6195&rep=rep1&type=pdf)
+# 8. [Motter, A. E: and Y. C. Lai (2002), *Cascade-based attacks on complex networks*, Physical Review E **66**, p. 065102.](http://arxiv.org/pdf/cond-mat/0301086)
+# 9. [Watts, J. and H. Strogatz (1998), *Collective dynamics of 'small-world' networks*, Nature **393**, p. 440.](http://www.nature.com/nature/journal/v393/n6684/pdf/393440a0.pdf)
+# 10. [*OpenCellID* (ENAiKOON).](http://opencellid.org/)
+# 11. [*Mozilla Location Service* (Mozilla Foundation).](https://location.services.mozilla.com/)
+# 
 # 
 
 # ##Bibliografia   
